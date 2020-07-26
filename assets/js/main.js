6bc29135acc185cd1e75770970333b59597a515e
krystofpiorecky <krystofpiorecky@gmail.com>
2020-07-26 10:13:33 +0200

    Follow mouse

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,8 @@ let mouse = {
 	y: 0
 };
 
+let last_mouse = {};
+
 let t = Math.PI*60;
 let spinning = true;
 let mousedown = false;
@@ -37,7 +39,7 @@ window.onload = e =>
 	// );
 
 	icube.rotateX(Math.PI/4);
-	icube.rotateY(Math.PI/3);
+	icube.rotateY(Math.PI/5);
 	// icube.rotateZ(Math.PI/4);
 
 	canvas.width = 1000;
@@ -61,14 +63,20 @@ window.onmousemove = e => {
 };
 
 window.onmousedown = e => {
-	spinning = false;
-	mousedown = true;
+	// spinning = false;
+	// mousedown = true;
+
+	// console.log(icube.rotation);
+
+
+	// icube.rotateX(-icube.rotation.x);
+	// icube.rotateY(-icube.rotation.y);
 };
 
-window.onmouseup = e => {
-	mousedown = false;
-	spinning = true;
-};
+// window.onmouseup = e => {
+// 	mousedown = false;
+// 	spinning = true;
+// };
 
 function draw()
 {
@@ -80,8 +88,8 @@ function draw()
 
 	if(spinning)
 	{
-		icube.rotateY(-Math.PI/2000 * (Math.sin(t/30))); //icube.rotateY(Math.PI/200 * (Math.sin(t/30)-1));
-		icube.rotateX(Math.PI/2000 * (Math.cos(t/30)));
+		// icube.rotateY(-Math.PI/2000 * (Math.sin(t/30))); //icube.rotateY(Math.PI/200 * (Math.sin(t/30)-1));
+		// icube.rotateX(Math.PI/2000 * (Math.cos(t/30)));
 
 		// icube.rotateY(Math.PI/200);
 		// icube.rotateX(Math.PI/300);
@@ -90,6 +98,31 @@ function draw()
 
 		// icube.rotateZ(Math.PI/12);
 	}
+
+	let screen = new Vector2(
+		window.innerWidth,
+		window.innerHeight
+	);
+
+	let amount = (distance) => {
+		if(distance < 0) return -amount(-distance);
+		return Math.pow(distance/100,1/2)/10;
+	};
+
+	if(last_mouse.x != undefined)
+	{
+		icube.rotateY(-amount(last_mouse.y-screen.y/2));
+		icube.rotateX(-amount(last_mouse.x-screen.x/2));
+	}
+
+	if(mouse.x != undefined)
+	{
+		icube.rotateX(amount(mouse.x-screen.x/2));
+		icube.rotateY(amount(mouse.y-screen.y/2));
+	}
+
+	last_mouse.x = mouse.x;
+	last_mouse.y = mouse.y;
 
 	t+=0.1;
 }
